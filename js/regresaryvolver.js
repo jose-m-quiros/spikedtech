020b720a7735cadc168a632c368372eb62d7e8f3
jose-m-quiros <jqchaves1928@gmail.com>
2025-01-12 15:21:27 -0600

    Version 110

--- js/regresaryvolver.js
+++ js/regresaryvolver.js
@@ -1,7 +1,3 @@
 document.getElementById("back-btn").addEventListener("click", () => {
   window.history.back();
 });
-
-document.getElementById("forward-btn").addEventListener("click", () => {
-  window.history.forward();
-});
