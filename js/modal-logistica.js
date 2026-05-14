a746d5a9cd430a8c9d01bb45d15748eff18fa143
JM QUIROS <jqchaves1928@gmail.com>
2026-05-14 16:03:40 -0600

    Remove deprecated loading, modal, and navbar scripts to streamline codebase

--- js/modal-logistica.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".platform-btn").forEach((button) => {
-  button.addEventListener("click", () => {
-    const platformLink = button.getAttribute("data-link");
-    window.open(platformLink, "_blank");
-  });
-});
